13b2b287c1f2c2fb80f34d8741b2908537e0e757
Vincent <132348214+yeungpakming@users.noreply.github.com>
2024-09-08 15:21:09 +0800

    Update mouse_controller.py

--- mouse_controller.py
+++ mouse_controller.py
@@ -1,13 +1,35 @@
 import pyautogui
+import math
+
 
 class mouse_controller:
+    def __init__(self, smoothing_factor=0.3, actuation_distance=50):
+        self.smoothing_factor = smoothing_factor
+        self.screen_size = pyautogui.size()
+        self.scaling_factor = math.sqrt(self.screen_size[0] * self.screen_size[1])
+        self.x = 0.5 * self.screen_size[0]
+        self.y = 0.5 * self.screen_size[1]
+        self.actuation_distance = actuation_distance
+        self.distance = 100
+        self.click_status = True
+
+    def screen_position(self, position):
+        x, y = position
+        return ((1 - x) * self.screen_size[0], y * self.screen_size[1])
+
+    def screen_distance(self, distance):
+        return distance * self.scaling_factor
 
     def move(self, position):
-        x, y = position
-        pyautogui.moveTo(x, y, _pause=False)
+        new_x, new_y = self.screen_position(position)
+        new_x = self.smoothing_factor * new_x + (1 - self.smoothing_factor) * self.x
+        new_y = self.smoothing_factor * new_y + (1 - self.smoothing_factor) * self.y
+        self.x, self.y = new_x, new_y
+        pyautogui.moveTo(self.x, self.y, _pause=False)
 
-    def press(self):
-        pyautogui.mouseDown()
-
-    def release(self):
-        pyautogui.mouseUp()
+    def left_click(self, distance):
+        if self.distance > self.actuation_distance:
+            self.status = True
+        self.distance = self.screen_distance(distance)
+        if self.distance < self.actuation_distance and self.status:
+            pyautogui.click(_pause=False)

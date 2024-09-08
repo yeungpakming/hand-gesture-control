8d35112ddc346fb9633ff4a86d68465290a0bb3d
Vincent <132348214+yeungpakming@users.noreply.github.com>
2024-09-08 15:22:06 +0800

    Update gesture_control.py
    kinda working, but scuffed

--- gesture_control.py
+++ gesture_control.py
@@ -13,8 +13,6 @@ def main():
     label = hgr.landmark_label
     mouse = mc.mouse_controller()
 
-    # the time module is used for checking fps(i commented), i just copy this part of the code from somewhere :P so will remove upon finishing the project; or i'll write my own
-    # the text is flipped, because cv2.imshow flipping the frame, still readable so lazy to fix
     # cTime = 0
     # pTime = 0
 
@@ -24,16 +22,18 @@ def main():
             print("Ignoring empty camera frame.")
             continue
         frame = recognizer.hand_detector(frame)
-        mouse.move(recognizer.screen_position(label.MIDDLE_FINGER_MCP))
+        mouse.move(recognizer.get_position(label.MIDDLE_FINGER_MCP))
+        mouse.left_click(
+            recognizer.get_distance(label.INDEX_FINGER_TIP, label.THUMB_TIP)
+        )
 
+        #     # cTime = time.time()
+        #     # fps = 1 / (cTime - pTime)
+        #     # pTime = cTime
+        #     # cv2.putText(
+        #     #     frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3
+        #     # )
 
-        # cTime = time.time()
-        # fps = 1 / (cTime - pTime)
-        # pTime = cTime
-        # cv2.putText(
-        #     frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3
-        # )
-        
         cv2.imshow(window_name, cv2.flip(frame, 1))
 
     source.release()
